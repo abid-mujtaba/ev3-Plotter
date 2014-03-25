c15dfaf67a5db0c01ea3347ecf0648bf55c21c25
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-03-24 22:58:26 -0500

    Implements polar plotting method. Increases base speed. Updates assembly and rover lengths to move along complete x-axis and an equal amount along the y-axis. Removes 'left' and 'right' methods that are no longer needed.

--- src/Plotter.java
+++ src/Plotter.java
@@ -36,7 +36,7 @@ public class Plotter
     static RegulatedMotor assembly = Motor.B;
     static RegulatedMotor rover = Motor.C;
 
-    static final int SPEED = 50;            // The speed for all three motors
+    static final int SPEED = 100;            // The speed for all three motors
 
     // Define the various Button IDs (extracted from the LeJOS Button class) used with result of Button.waitForAnyEvent()
     static final int RELEASE_EVENT_SHIFT = 8;          // Bit shift required to convert button down to button up
@@ -56,13 +56,11 @@ public class Plotter
     static final int CENTER_Press = 0x2;
 
 
-
-    static final int ASSEMBLY_LENGTH = 120;     // Rotation angle for the assembly that makes it move across it's entire length
+    static final int ASSEMBLY_LENGTH = 180;     // Rotation angle for the assembly that makes it move across it's entire length
     static final int PEN_LENGTH = 40;           // Angle that lifts pen sufficiently
-    static final int ROVER_LENGTH = 75;         // Angle that moves rover forward equal to the full range of motion of the assembly
+    static final int ROVER_LENGTH = 100;         // Angle that moves rover forward equal to the full range of motion of the assembly
 
     static int mAssemblyPos = 0;            // Keeps track of the current position of the assembly
-
 
 
     public static void main(String[] args)
@@ -141,16 +139,17 @@ public class Plotter
                     flag = false;
             }
         }
+
+        // After a successful adjustment the assembly is at position ASSEMBLY_LENGTH (all the way to the right) and is prepared for limit tracking.
+        mAssemblyPos = 0; //ASSEMBLY_LENGTH;
     }
 
     // Method that tells the EV3 what to plot.
     private static void plot()
     {
-        left(80, false);
-        forward(80, false);
-
-        right(80, true);
-        reverse(80, false);
+        polar(10, 180);      // Move left
+        polar(10, 90);       // Move forward
+        polar(14.14, 315);   // Move diagonally back to the origin
 
         raise_pen();
     }
@@ -168,24 +167,74 @@ public class Plotter
     }
 
 
-    private static void right(int percent, boolean immediate_return)
+    /** Controls the inherently x-y plotter in a polar fashion by adjusting lengths and speeds
+     *
+     * The assembly and rover wheels have different radii so they move by different absolute amounts for the same angular rotation. We take the maximum possible movement by the assembly to be equal to 10 UNITs of motion.
+     *
+     * The relation between angles and absolute distances in terms of UNITs is given by:
+     *
+     *      Assembly: 10 UNITs = 120
+     *      Rover:    10 UNITs =  75
+     *
+     * This method takes magnitude in UNITs and an angle in degrees and uses it to calculate the SPEEDs and angles that will move the pen in the specified polar direction.
+     */
+
+    private static int UNITS = 10;         // The number of standard units per max assembly motion. (The number of UNITs that corresponds to the assembly moving across its entire length)
+
+    private static void polar(double mag, int degree_angle)
     {
-        move_assembly(-percent, immediate_return);
+        // We use trigonometry to calculate the x and y movement the pen must make in UNITs
+        double angle = degree_angle / 180.0 * Math.PI;
+
+        double x = mag * Math.cos(angle);
+        double y = mag * Math.sin(angle);
+
+        // With x and y UNITs in hand we use them to calculate the speeds and angles of the motors. Angles first:
+        int x_angle = (int) (x / UNITS * ASSEMBLY_LENGTH);
+        int y_angle = (int) (y / UNITS * ROVER_LENGTH);
+
+        // The SPEEDs need to be adjusted so that the maximum value is SPEED (set globally) and so that both x and y motions start and (more importantly) end at the same time so it is about matching the "time"
+        // of motions now that the displacement has been set. Since speed = distance / time => time = distance / speed. and so to match times the SPEEDs must be in the same ratio as the distances (angles).
+
+        // We just need to ensure that the larger of the two speeds is equal to the global value SPEED:
+
+        int xSpeed, ySpeed;
+
+        if (Math.abs(x_angle) > Math.abs(y_angle))
+        {
+            xSpeed = SPEED;
+
+            ySpeed = (int) Math.abs((double) y_angle / x_angle * SPEED);
+        }
+        else
+        {
+            ySpeed = SPEED;
+
+            xSpeed = (int) Math.abs((double) x_angle / y_angle * SPEED);
+        }
+
+        // With the calculations complete we simply set the necessary speeds and rotate by the calculated angles.
+
+        assembly.setSpeed(xSpeed);
+        rover.setSpeed(ySpeed);
+
+        if (y_angle == 0)           // if the y_angle is 0 then move_assembly must NOT return immediately since that would cause the execution to effectively skip this instruction
+        {
+            move_assembly(-x_angle, false);
+        }
+        else
+        {
+            move_assembly(-x_angle, true);      // We need the negative sign because our coordinate system (x-y) and its positive directions correspond to the negative rotational direction of both motors
+            rover.rotate(-y_angle, false);
+        }
     }
 
 
-    private static void left(int percent, boolean immediate_return)
+    private static void move_assembly(int angle, boolean immediate_return)
     {
-        move_assembly(percent, immediate_return);
-    }
+        int new_pos = angle + mAssemblyPos;
 
-
-    private static void move_assembly(int percent, boolean immediate_return)
-    {
-        int angle = (int) (percent / 100.0 * ASSEMBLY_LENGTH);
-        int new_angle = angle + mAssemblyPos;
-
-        if (new_angle < 0 || new_angle > ASSEMBLY_LENGTH)           // The new motion will cause the assembly to come of the end of the tracks
+        if (new_pos < 0 || new_pos > ASSEMBLY_LENGTH)           // The new motion will cause the assembly to come of the end of the tracks
         {
             log("Error: Moving by this angle will cause assembly to move off tracks. Motion Denied.");
             return;
@@ -195,27 +244,6 @@ public class Plotter
 
         mAssemblyPos += angle;      // Update the current position of the assembly
     }
-
-
-    private static void forward(int percent, boolean immediate_return)
-    {
-        move_rover(-percent, immediate_return);
-    }
-
-
-    private static void reverse(int percent, boolean immediate_return)
-    {
-        move_rover(percent, immediate_return);
-    }
-
-
-    private static void move_rover(int percent, boolean immediate_return)
-    {
-        int angle = (int) (percent / 100.0 * ROVER_LENGTH);
-
-        rover.rotate(angle, immediate_return);
-    }
-
 
 
     // Methods for controlling all three of the motors in an abstract fashion:
